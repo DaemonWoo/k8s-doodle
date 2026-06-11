4627a21aac641b8967a8e5125990ec2609d4a8e5
DaemonWoo <ivanovzakhary@gmail.com>
2026-06-11 20:49:10 +0300

    Add k8s configmap

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # k8s-doodle
 A simple Flask application deployed to Kubernetes.
+
+## Technologies
+- Python
+- Flask
+- Docker
+- Kubernetes
+- Nginx Ingress
--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello():
+    return os.getenv("GREETING", "Hello, world!")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
