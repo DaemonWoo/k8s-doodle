92f448c1bf5d06b6eb985502de3b63becc7bf38b
DaemonWoo <ivanovzakhary@gmail.com>
2026-06-16 11:12:15 +0300

    Add k8s persistent volume clain

--- hello.py
+++ hello.py
@@ -4,11 +4,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+FILE = "/data/message.txt"
 
 @app.route("/")
 def hello():
-    return os.getenv("GREETING", "Hello, world!")
+    greet = os.getenv("GREETING", "Hello, world!")
 
+    os.makedirs(os.path.dirname(FILE), exist_ok=True)
+    
+    if not os.path.exists(FILE):
+        with open(FILE, "w") as f:
+            f.write("Hello from Persistent Volume!")
+
+    with open(FILE) as f:
+        return f.read()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
